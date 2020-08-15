f11002f62cd4f64e99b4d1b820f70a649ed8ebac
umairayub79 <umairayub79@gmail.com>
2020-08-15 20:28:27 +0500

    Sorted List by LastModified() to put latest files on top

--- app/src/main/java/codes/umair/wastatussaver/Utils.kt
+++ app/src/main/java/codes/umair/wastatussaver/Utils.kt
@@ -1,6 +1,9 @@
 package codes.umair.wastatussaver
 
 import java.io.File
+import java.util.*
+import kotlin.collections.ArrayList
+
 
 class Utils {
     constructor()
@@ -23,7 +26,17 @@ class Utils {
 
             }
         }
-        return inFiles
+        return sortList(inFiles)
     }
 
+    private fun sortList(list: ArrayList<File>): ArrayList<File> {
+        Collections.sort(list, FileDateComparator())
+        return list
+    }
+
+    class FileDateComparator : Comparator<File> {
+        override fun compare(file: File, file1: File): Int {
+            return file1.lastModified().compareTo(file.lastModified())
+        }
+    }
 }
